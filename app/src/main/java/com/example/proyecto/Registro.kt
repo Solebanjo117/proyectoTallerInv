5ed6ecbdb4f6723a86425209c36d018d4a0c8310
JuanAntonio <jantoni0@outlook.com>
2025-04-16 03:52:54 +0000

    vistas

--- app/src/main/java/com/example/proyecto/Registro.kt
+++ app/src/main/java/com/example/proyecto/Registro.kt
@@ -2,16 +2,15 @@ package com.example.proyecto
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<TextView>(R.id.textRegistro).setOnClickListener {
-            val intent = Intent(this, Registro::class.java)
+        setContentView(R.layout.registro) // Aquí enlazas tu layout
+        findViewById<Button>(R.id.btnVolverLogin).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
